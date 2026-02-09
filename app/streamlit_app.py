bded865d42e1d5b70db2557d4467e814ca6529a9
Marwane ELM <pro.marwane.elm@gmail.com>
2026-02-09 21:34:29 +0100

    changements

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -228,7 +228,7 @@ if search:
         st.session_state.address_valid = False
 
     else : 
-        geolocator = Nominatim(user_agent=" pro.marwane.elm@gmail.com")
+        geolocator = Nominatim(user_agent="streamlit_app", timeout=5)
         location = geolocator.geocode(address, addressdetails=True, exactly_one=True)
         
 
